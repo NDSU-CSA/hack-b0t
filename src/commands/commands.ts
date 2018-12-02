994e2116bd64dac8ab0dee021e2c047aa3316522
Jack Hance <Jack_e_hance@yahoo.com>
2018-12-02 03:40:05 -0600

    Quick fix to blur command, updated example auth file

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -88,7 +88,7 @@ export const commands : {[key: string] : ICommand} = {
     "invert": {
         name: "invert",
         description: "Inverts the colors of the most recent sent image",
-        category: "fun",
+        category: "image",
         usage: "invert",
         admin: false,
         process: (params: ICommandParams) : void  => {
@@ -122,6 +122,58 @@ export const commands : {[key: string] : ICommand} = {
             .catch((err : Error) => {
                 console.log(err);
                 params.bot.postMessage(params.message.channel, "Unable to invert image");
+            });
+        }
+    },
+    "blur": {
+        name: "blur",
+        description: "blur the most recent sent image by a given amount",
+        category: "image",
+        usage: "blur [amt, default 5]",
+        admin: false,
+        process: (params: ICommandParams) : void  => {
+            if(!params.message.text) return;
+
+            let blurAmt : number = 5;
+            if(params.message.text.split(" ").length >= 2) {
+                let amt : string = params.message.text.split(" ")[1];
+                if(!parseInt(amt)) {
+                    params.bot.postMessage(params.message.channel, "Invalid blur amount");
+                    return;
+                } else {
+                    blurAmt = parseInt(amt);
+                }
+            }
+
+            params.user.files.list({
+                channel: params.message.channel,
+                types: "images",
+                count: 1
+            })
+            .then((response : any) => {
+                let downloadParams : any = {
+                    url: response.files[0].url_private,
+                    headers: {
+                        "Authorization" : `Bearer ${params.userToken}`
+                    }
+                };
+                return Jimp.read(downloadParams);
+            })
+            .then((image : Jimp) => image.blur(blurAmt).getBufferAsync(Jimp.MIME_JPEG))
+            .then((buffer : Buffer) => {
+                let stream : Duplex = new Duplex();
+                stream.push(buffer);
+                stream.push(null);
+
+                return params.slack.files.upload({
+                    filename: `${Math.floor(Math.random() * 5000) + 1000}`,
+                    file: stream,
+                    channels: params.message.channel
+                });
+            })
+            .catch((err : Error) => {
+                console.log(err);
+                params.bot.postMessage(params.message.channel, "Unable to blur image");
             });
         }
     },
